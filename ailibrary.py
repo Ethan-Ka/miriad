17f269f90b0aa196c0b7b210e6ba1dde91de1276
Ethan K <hambugerethan@gmail.com>
2022-10-04 11:48:39 -0500

    Update ailibrary.py

--- ailibrary.py
+++ ailibrary.py
@@ -8,6 +8,25 @@ class AILibrary:
         openai.organization = "org-sUgNqRTwwVZMVytUjb0LYp6b"
         openai.api_key = API_KEY
         openai.Model.list()
+    def debug(self, prompt):
+      response = openai.Completion.create(
+      model="code-davinci-002",
+      prompt=f"##### Fix bugs in the below function\n \n### Buggy Python\n{prompt}\n### Fixed Python",
+      temperature=0,
+      max_tokens=500,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0,
+      stop=["###"]
+            
+      )
+      list = {
+          0:interaction["choices"][0]["text"], 
+          1:{json.dumps(interaction)},
+          2:interaction["choices"][0]["finish_reason"]
+        }
+      return list
+
     def text(self, prompt, model):
         model3 = model
         interaction = openai.Completion.create(
